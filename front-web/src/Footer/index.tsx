2ff6857a633cd2d746a4a623fe27d54832700e84
georgepiter <george.piter@terra.com.br>
2021-01-09 16:34:25 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,31 @@
+import './styler.css'
+import { ReactComponent as YoutubeIcon } from './youtube.svg'
+import { ReactComponent as LinkedinIcon } from './linkedin.svg'
+import { ReactComponent as InstagramIcon } from './instagram.svg'
+
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+          <div className="footer-icons">
+           <a href="linkaqui" target="_new">
+               <YoutubeIcon />
+
+           </a>
+           <a href="https://www.linkedin.com/in/gpiter" target="_new">
+              <LinkedinIcon />
+
+           </a>
+
+           <a href="linkaqui" target="_new">
+              <InstagramIcon />
+
+           </a>
+
+          </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,27 +1,31 @@
-import { ReactComponent as MainImage } from './main.svg';
 import './style.css';
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home() {
     return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br /> que entregamos <br /> pra você!!!
-                    </h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos <br />
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br /> que entregamos <br /> pra você!!!
+                       </h1>
+                        <h3 className="home-subtitle">
+                            Escolha o seu pedido e em poucos minutos <br />
                         levaremoss na sua porta
-                    </h3>
-                    <a href="orders" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
-                </div>
-                <div className="home-image">
-                    <MainImage />
+                       </h3>
+                        <a href="orders" className="home-btn-order">
+                            FAZER PEDIDO
+                       </a>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
