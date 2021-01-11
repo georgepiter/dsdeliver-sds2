bacf2692b3022f6aa07ce013a22fc66e1c4bae25
georgepiter <george.piter@terra.com.br>
2021-01-11 01:13:50 -0300

    Correção label em index/home-subtitle

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -15,7 +15,7 @@ function Home() {
                        </h1>
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
-                            levaremoss na sua porta
+                            levaremos na sua porta
                        </h3>
                         <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
